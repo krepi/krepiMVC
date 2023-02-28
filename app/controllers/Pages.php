9c48d23caaa5004e43e01518ca42d1255dadd0a0
Przemek <krepski80@gmail.com>
2023-02-28 19:32:09 +0100

    Base Controller class created

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,8 +1,22 @@
 <?php
 
-class Pages {
+class Pages extends Controller
+{
     public function __construct()
     {
-        echo 'pages loaded';
+    }
+
+
+    public function index()
+    {
+        $data = [
+            'title' => 'welcome'
+        ];
+        $this->view('pages/index', $data);
+    }
+
+    public function about()
+    {
+        $this->view('pages/about');
     }
 }
