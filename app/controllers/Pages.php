b3f80209d1c7b3a8dc894d9d48a1cb10bc09b1dd
Przemek <krepski80@gmail.com>
2023-03-01 21:52:09 +0100

    first MVC project done

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,15 +4,17 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel = $this->model('Post');
     }
-
 
     public function index()
     {
+
         $data = [
-            'title' => 'Welcome'
+            'title' => 'Welcome',
+
         ];
+
+
         $this->view('pages/index', $data);
     }
 
