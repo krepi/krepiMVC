af5fb08488082efde31fa2f888c8e2abfa85efc1
Przemek <krepski80@gmail.com>
2023-03-01 20:43:14 +0100

    database class made

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,12 @@
 <?php
+//DB params
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','kmvc');
+
+
 //app root
 define('APPROOT', dirname(dirname(__FILE__)));
 //URL Root
---
+++ app/libraries/database.php
@@ -0,0 +1,66 @@
+<?php
+
+/** PDO Database Class
+ * connect to db
+ * create prepared statements
+ * bind values
+ * returns rows and results
+ */
+
+class Database
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct()
+    {
+        //set dsn
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        //create new PDO instance
+
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+    //Prepare statement with query
+    public function query($sql)
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    //bind vales 
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+                    
+            }
+        }
+    }
+}
