118cc95ca256b13fdf7953bee927ce6cb904f088
Przemek <krepski80@gmail.com>
2023-03-08 12:11:40 +0100

    first routes table created

--- public/index.php
+++ public/index.php
@@ -5,4 +5,24 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+// echo get_class($router);
+
+// add the routes
+
+$router->add('',['controller' =>'Home', 'action'=>'index']);
+$router->add('posts',['controller' =>'Posts', 'action'=>'index']);
+$router->add('posts/new',['controller' =>'Posts', 'action'=>'new']);
+
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '<pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)){
+    echo '<pre>';
+var_dump($router->getParams());
+echo '<pre>';
+} else {
+    echo 'not route for URL '.$url ;
+}
