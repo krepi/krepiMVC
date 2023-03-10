f1a3ab05622f1a1acf28f255784b7836dc8fbb5a
Przemek <krepski80@gmail.com>
2023-03-10 20:31:31 +0100

    loadin classes by the composer , added the autoload field to composer.json

--- public/index.php
+++ public/index.php
@@ -9,18 +9,18 @@
  * Twig
  * */
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-
+//require dirname(__DIR__) . '/vendor/autoload.php';
+require '../vendor/autoload.php';
 /**
  * Autoloader
  */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);   // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
+//spl_autoload_register(function ($class) {
+//    $root = dirname(__DIR__);   // get the parent directory
+//    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+//    if (is_readable($file)) {
+//        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+//    }
+//});
 
 
 /**
