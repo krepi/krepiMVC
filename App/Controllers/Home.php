a107689d5cebfd3f2cccd87f60d3d3a50b5d536e
Przemek <krepski80@gmail.com>
2023-03-10 18:36:44 +0100

    Home View index added

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 /**
  * Home controller
  *
@@ -17,7 +19,7 @@ class Home extends \Core\Controller
      */
     protected function before()
     {
-        echo "(before) ";
+//        echo "(before) ";
         //return false;
     }
 
@@ -28,7 +30,7 @@ class Home extends \Core\Controller
      */
     protected function after()
     {
-        echo " (after)";
+//        echo " (after)";
     }
 
     /**
@@ -38,6 +40,9 @@ class Home extends \Core\Controller
      */
     public function indexAction()
     {
-        echo 'Hello from the index action in the Home controller!';
+        View::render('Home/index.php', [
+            'name' => 'Dave',
+            'colours' => ['red', 'green', 'blue']
+        ]);
     }
 }
