b5dd87c331cc144a0d0dd27f34890d85be47ed29
Przemek <krepski80@gmail.com>
2023-03-11 18:19:58 +0100

    config file with database datas added

--- Core/Model.php
+++ Core/Model.php
@@ -1,25 +1,30 @@
 <?php
 
 namespace Core;
+
 use PDO;
 use PDOException;
- abstract class Model
+use App\Config;
+
+abstract class Model
 {
 
-    protected static function getDB(){
-         static $db = null;
-         if($db === null) {
-             $host = 'localhost';
-             $dbname = 'kmvc';
-             $username = 'root';
-             $password = '';
-             try {
-                 $db = new PDO("mysql:host=$host; dbname=$dbname;charset=utf8", $username, $password);
-             } catch(PDOException $e){
-                 echo $e->getMessage();
-             }
-         }
-         return $db;
+    protected static function getDB()
+    {
+        static $db = null;
+        if ($db === null) {
+//             $host = 'localhost';
+//             $dbname = 'kmvc';
+//             $username = 'root';
+//             $password = '';
+            try {
+                $dsn = 'mysql:host=' . Config::DB_HOST . '; dbname=' . Config::DB_NAME . '; charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+        return $db;
     }
 
 
