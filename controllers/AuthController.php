e600b546bd54da854ee1c90b3dad09433ea86910
Przemek <krepski80@gmail.com>
2023-03-15 21:26:00 +0100

    DbModela created and RegisterModel name changed to User

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace app\controllers;
 use app\core\Controller;
 
 use app\core\Request;
-use app\models\RegisterModel;
+use app\models\User;
 
 /**
  *
@@ -21,7 +21,7 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $registerModel = new RegisterModel();
+        $registerModel = new User();
         if($request->isPost()){
 
             $registerModel->loadData($request->getBody());
